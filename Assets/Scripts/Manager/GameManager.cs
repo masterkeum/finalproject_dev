8723b379193f85341f7e9e638c23d0b7329e93af
masterkeum <masterkeum@gmail.com>
2024-03-12 19:21:55 +0900

    fix: gamemanager 로 프리팹 생성

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,28 @@ using UnityEngine;
 public class GameManager : SingletoneBase<GameManager>
 {
     [ReadOnly, SerializeField] private string _pidStr;
+    private GameObject player;
+    [SerializeField] private Transform playerParent;
+    private VariableJoystick joyStick;
+    [SerializeField] private Transform parentCanvas;
+    
 
     protected override void Init()
     {
         _pidStr = _pid.ToString();
         base.Init();
-
         // 초기화
+    }
 
+    private void Start()
+    {
+        MakePlayer();
+    }
+
+    private void MakePlayer()
+    {
+        player = Instantiate(Resources.Load<GameObject>("Prefabs/Player/man_casual_shorts"), playerParent);
+        joyStick = Instantiate(Resources.Load<VariableJoystick>("Prefabs/Joystick/VariableJoystick"), parentCanvas);
+        player.GetComponent<Player>().JoyStick(joyStick);
     }
 }
