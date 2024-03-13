0990d646860eb623ebda1af3120b3d8525cb9124
soonseong.k <taetae8939@gmail.com>
2024-03-13 12:15:03 +0900

    Fix. 주석 정리

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -6,26 +6,28 @@ public class GameManager : SingletoneBase<GameManager>
 {
     [ReadOnly, SerializeField] private string _pidStr;
 
-
-    public Transform Player { get; private set; }
-
     /*
-        진입한 스테이지ID
-        몬스터 생성
+    게임매니저
+        진입한 스테이지ID 가지고있게
+        
+        해당하는 스테이지 필드 생성
+        플레이어 생성
+        게임 진행 관련 기본 세팅 (타이머, 레벨, 경험치 등 초기화)
 
+        게임 진행 로직
+            보스몬스터 생성
+            몬스터 생성 시작
+            레벨업
+            게임 완료 처리
+
+        일시정지 로직
     */
 
     protected override void Init()
     {
         _pidStr = _pid.ToString();
         base.Init();
-
         // 초기화
-
-        // TODO : 플레이어 생성. 생성 후 player에 넣어주기
-
     }
-
-
 
 }
