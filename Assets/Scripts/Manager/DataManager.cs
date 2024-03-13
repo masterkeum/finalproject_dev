2a1a222236aa2b393658155f5e205c02f31e1c29
soonseong.k <taetae8939@gmail.com>
2024-03-13 11:32:31 +0900

    Feat. Data매니저 작성

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -1,3 +1,5 @@
+using Gley.Localization.Internal;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,93 +7,106 @@ public class DataManager : SingletoneBase<DataManager>
 {
     [ReadOnly, SerializeField] private string _pidStr;
 
-    // 데이터 파일 경로
-    //private string unitDataFilePath = "DataTable/UnitTable.csv";
-    //public Dictionary<int, UnitData> itemDataDictionary { get; private set; }
-    private string unitDataFilePath = "DataTable/QuestInfoTable.csv";
-    public Dictionary<int, Quest> QuestDataDictionary { get; private set; }
+    // 데이터 파일 경로 Assets/Resources/DataTables/DataTable.json
+    private string DataFilePath = "DataTables/DataTable";
+
+    public Dictionary<int, CharacterInfo> characterInfoDict;
+    public Dictionary<int, MonsterLevel> monsterLevelDict;
 
     protected override void Init()
     {
         _pidStr = _pid.ToString();
         base.Init();
-
-
-        /*
-            데이터 로드해서 세팅
-        
-            NOTE: 데이터를 어떻게 관리할 것인가.
-            1. Json 으로
-            2. CSV to SO https://github.com/mikito/unity-excel-importer?tab=readme-ov-file
-            3. csvHelper 등 외부 라이브러리
-            4. 스크립트 https://bravenewmethod.com/2014/09/13/lightweight-csv-reader-for-unity/
-                https://fkdl0048.github.io/unity/unity_in_CSV/
-
-        */
-
-        // 데이터 관리
-        //ReadUnitData();
-        //QuestDataDictionary = CSVReader.Read(unitDataFilePath);
-
-        // 프리팹 관리
+        LoadJsonData();
     }
 
 
-    private void ReadUnitData()
+    private void LoadJsonData()
     {
-        //itemDataDictionary = new Dictionary<int, UnitData>();
-        TextAsset TextFile = Resources.Load<TextAsset>(unitDataFilePath);
-
-
-
-
-        //string[] lines = File.ReadAllLines(unitDataFilePath);
-        //for (int i = 3; i < lines.Length; i++)
-        //{
-        //    string[] fields = lines[i].Split(',');
-        //    int uid = int.Parse(fields[0]);
-        //    string character = fields[1];
-        //    string nameAlias = fields[2];
-        //    string descAlias = fields[3];
-        //    int HP = int.Parse(fields[4]);
-        //    int atk = int.Parse(fields[5]);
-        //    string prefabFileName = fields[6];
-        //    string prefabFilePath = fields[7];
-        //    int defaultCapacity = int.Parse(fields[8]);
-        //    int maxSize = int.Parse(fields[9]);
-
-        //    UnitData unitData = new UnitData(uid, character, nameAlias, descAlias, HP, atk, prefabFileName, prefabFilePath, defaultCapacity, maxSize);
-
-        //    itemDataDictionary.Add(uid, unitData);
-        //}
+        TextAsset jsonFile = Resources.Load<TextAsset>(DataFilePath);
+        if (jsonFile != null)
+        {
+            //string jsonString = jsonFile.text;
+            ConvertJsonToDictionary(jsonFile.text);
+        }
+        else
+        {
+            Debug.LogError("Failed to load JSON file at path: " + DataFilePath);
+        }
     }
 
+    public void ConvertJsonToDictionary(string json)
+    {
+        Root jsonData = JsonUtility.FromJson<Root>(json);
+
+        characterInfoDict = new Dictionary<int, CharacterInfo>();
+        monsterLevelDict = new Dictionary<int, MonsterLevel>();
+
+        foreach (CharacterInfo characterInfo in jsonData.CharacterInfo)
+        {
+            characterInfoDict.Add(characterInfo.uid, characterInfo);
+            Debug.Log(characterInfo.uid);
+        }
+        foreach (MonsterLevel monsterLevel in jsonData.MonsterLevel)
+        {
+            characterInfoDict[monsterLevel.uid].monsterLevelData.Add(monsterLevel);
+        }
+
+    }
 }
 
-public class UnitData
+
+public class Root
+{
+    public List<CharacterInfo> CharacterInfo;
+    public List<MonsterLevel> MonsterLevel;
+    public List<StageInfoTable> StageInfoTable;
+}
+
+
+[Serializable]
+public class CharacterInfo
 {
     public int uid;
-    public string character;
-    public string nameAlias;
-    public string descAlias;
-    public int HP;
-    public int atk;
-    public string prefabFileName;
-    public string prefabFilePath;
-    public int defaultCapacity;
-    public int maxSize;
+    public string characterType;
+    public string name;
+    public int minLevel;
+    public int maxLevel;
+    public int hp;
+    public int ap;
+    public int? sensoryRange;
+    public int attackRange;
+    public int attackSpeed;
+    public int moveSpeed;
+    public int defaultSkill;
+    public string prefabFile;
 
-    public UnitData(int _uid, string _character, string _nameAlias, string _descAlias, int _HP, int _atk, string _prefabFileName, string _prefabFilePath, int _defaultCapacity, int _maxSize)
-    {
-        uid = _uid;
-        character = _character;
-        nameAlias = _nameAlias;
-        descAlias = _descAlias;
-        HP = _HP;
-        atk = _atk;
-        prefabFileName = _prefabFileName;
-        prefabFilePath = _prefabFilePath;
-        defaultCapacity = _defaultCapacity;
-        maxSize = _maxSize;
-    }
+    public List<MonsterLevel> monsterLevelData;
+}
+
+
+[System.Serializable]
+public class MonsterLevel
+{
+    public int uid;
+    public int level;
+    public int addHP;
+    public int addAP;
+    public int gold;
+    public int exp;
+    public string rewardType;
+    public int? rewardID;
+}
+
+public class StageInfoTable
+{
+    public int stage;
+    public int uid;
+    public int level;
+    public string characterType;
+    public int genTimeStart;
+    public int genTimeEnd;
+    public int genAmount;
+    public int genTime;
+    public int genMax;
 }
