a2d4528942130bd6bad8a0d3424201d5dad693db
hamja900 <luru@naver.com>
2024-03-14 14:07:35 +0900

    feat. ingameData

--- Assets/Scripts/UI/InGameHUD.cs
+++ Assets/Scripts/UI/InGameHUD.cs
@@ -12,29 +12,31 @@ public class InGameHUD : MonoBehaviour
     public TextMeshProUGUI timeText;
     public TextMeshProUGUI curLevelText;
 
-    
+    private PlayerIngameData player;
 
 
     private void Start()
     {
-
+        player = GetComponent<PlayerIngameData>();
 
     }
 
     public void SetWhenStart()
     {
-        
+        expSlider.value = 0;
     }
 
     public void UpdateWhenGetGold()
     {
         //인게임 획득 골드 표시 업데이트
-
+        goldText.text = player.gold.ToString();
     }
     public void UpdateWhenEnemyDie()
     {
         //킬카운트와 인게임 경험치슬라이더,인게임 레벨 표시 업데이트
-
+        killText.text = player.killCount.ToString();
+        expSlider.value = player.sliderCurExp / player.sliderMaxExp;
+        curLevelText.text = player.curLevel.ToString();
     }
     public void OnPauseButton()
     {
