ca10ff16475d08139c872bcfca6f2f660f9612ab
hamja900 <luru@naver.com>
2024-03-17 20:05:41 +0900

    feat : mimic gacha

---
+++ Assets/Scripts/UI/UIEquipInfo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIEquipInfo : UIBase
+{
+    public TextMeshProUGUI newItemName;
+    public TextMeshProUGUI newItemStats;
+    public TextMeshProUGUI curItemText;
+    public TextMeshProUGUI curItemStats;
+    public Image newItemIcon;
+    public Image curItemIcon;
+
+}
