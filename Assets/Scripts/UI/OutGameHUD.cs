c6023d1168074c4a7fd0e89239d154a85d286a0b
hamja900 <luru@naver.com>
2024-03-17 22:06:35 +0900

    fix 메뉴 분리 완료

--- Assets/Scripts/UI/OutGameHUD.cs
+++ Assets/Scripts/UI/OutGameHUD.cs
@@ -15,10 +15,16 @@ public class OutGameHUD : MonoBehaviour
     public TextMeshProUGUI gemQuantityText;
     public TextMeshProUGUI goldQuantityText;
 
+    [Header("MainMenu")]
+    public GameObject[] mainMenu;
+
+    [Header("Bottom")]
+    public RectTransform[] buttonRect;
+
 
     private void Awake()
     {
-        UIManager.Instance.ShowUI<UIBattle>();
+        ChangeMenu(0);
     }
     private void Start()
     {
@@ -30,5 +36,27 @@ public class OutGameHUD : MonoBehaviour
 
     }
 
-   
+    public void ChangeMenu(int index)
+    {
+        for(int i = 0; i < mainMenu.Length; i++)
+        {
+            if (i == index)
+            {
+                mainMenu[i].SetActive(true);
+            }
+            else
+            {
+                mainMenu[i].SetActive(false);
+            }
+
+        }
+        for (int i = 0; i < buttonRect.Length; i++)
+        {
+            buttonRect[i].sizeDelta = new Vector2(i == index ? 450 : 300, buttonRect[i].sizeDelta.y);
+        }
+        buttonRect[0].pivot = new Vector2(index ==2? 1.5f : 1, 0);
+
+    }
+
+
 }
