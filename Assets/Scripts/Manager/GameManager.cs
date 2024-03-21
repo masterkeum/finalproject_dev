9ff1972b569e95c6ca58545872c12e0d857fc491
masterkeum <masterkeum@gmail.com>
2024-03-21 12:28:21 +0900

    feat: gamemanager player 객체 갖고있게

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -8,9 +8,11 @@ public class GameManager : SingletoneBase<GameManager>
     // 필요한가?
     public Transform playerParent;
     public Transform parentCanvas;
+    private Player player;
 
     // 씬이 넘어가도 유지할 데이터
     public int stageId { get; set; } // 진입한 스테이지ID 가지고있게
+    
 
 
     protected override void Init()
@@ -18,6 +20,8 @@ public class GameManager : SingletoneBase<GameManager>
         _pidStr = _pid.ToString();
         base.Init();
         // 초기화
+        
+        
 
 
         // 계정 세팅
@@ -25,6 +29,13 @@ public class GameManager : SingletoneBase<GameManager>
 
         stageId = 101; // !NOTE : Test코드
     }
+    
+    // 받을 메서드
+    public void TakePlayer(Player player)
+    {
+        this.player = player;
+    }
+
 
 
 }
