30bee32fdb768ea5d835eb68b3f3d2320297635f
hamja900 <luru@naver.com>
2024-04-04 15:01:42 +0900

    fix : 샵 슬롯 수정 진행중

--- Assets/Scripts/UI/Shop/ShopSlotUI.cs
+++ Assets/Scripts/UI/Shop/ShopSlotUI.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopSlotUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image icon;
+    public TextMeshProUGUI amount;
+    public TextMeshProUGUI price;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool isForSale = true;
 }
