1402641fb70efc4348cb00a85b083634fe7df983
masterkeum <masterkeum@gmail.com>
2024-04-11 21:18:51 +0900

    feat: 값 연동 코어값 가져오기

--- Assets/Scripts/UI/UIGameClear.cs
+++ Assets/Scripts/UI/UIGameClear.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -17,24 +18,51 @@ public class UIGameClear : UIBase
     public TextMeshProUGUI getHeroJS;
     public ItemTable itemInfos;
     private AccountInfo accountInfo;
+    private MonsterLevel monsterlevel1;
+    private MonsterLevel monsterlevel2;
+    private MonsterLevel monsterlevel3;
 
     private void Start()
     {
         player = GameManager.Instance.player;
         itemInfos = DataManager.Instance.itemTableDict[50000003];
+        List<MonsterLevel> monaterlevel = DataManager.Instance.characterInfoDict[20100001].monsterLevelData.Values.ToList();
         
-        // todo core 데이터 부분이 정해지지 않아 임의 데이터 삽입. 추후 수정.
-        Debug.Log("코어값 로드 "+itemInfos);
-        Debug.Log("코어값 getExp "+itemInfos.getExp);
-        Debug.Log("코어값 getGold "+itemInfos.getGold);
-        getGemText.text = itemInfos.getExp.ToString();
-        getHeroJS.text = itemInfos.getExp.ToString();
-        getNormalJS.text = itemInfos.getGold.ToString();
+        monsterlevel1 = DataManager.Instance.characterInfoDict[20100001].monsterLevelData[1];
+        monsterlevel2 = DataManager.Instance.characterInfoDict[20100001].monsterLevelData[2];
+        monsterlevel3 = DataManager.Instance.characterInfoDict[20100001].monsterLevelData[3];
         
-        // 팝업이 뜨면서 accountinfo 에 게임데이터 저장
-        accountInfo.AddGold(player.playeringameinfo.gold);
-        accountInfo.AddGem(itemInfos.getExp);
-        accountInfo.AddCore(itemInfos.getExp);
+        if (monsterlevel1.level == 1)
+        {
+            getGemText.text = itemInfos.getExp.ToString();
+            getHeroJS.text = itemInfos.getExp.ToString();
+            getNormalJS.text = itemInfos.getGold.ToString();
+            accountInfo.AddGold(player.playeringameinfo.gold);
+            accountInfo.AddGem(itemInfos.getExp);
+            accountInfo.AddCore(monsterlevel1.rewardAmount);
+            
+        }
+        if (monsterlevel2.level == 2)
+        {
+            getGemText.text = itemInfos.getExp.ToString();
+            getHeroJS.text = itemInfos.getExp.ToString();
+            getNormalJS.text = itemInfos.getGold.ToString();
+            accountInfo.AddGold(player.playeringameinfo.gold);
+            accountInfo.AddGem(itemInfos.getExp);
+            accountInfo.AddCore(monsterlevel2.rewardAmount);
+        }
+        if (monsterlevel3.level == 3)
+        {
+            getGemText.text = itemInfos.getExp.ToString();
+            getHeroJS.text = itemInfos.getExp.ToString();
+            getNormalJS.text = itemInfos.getGold.ToString();
+            accountInfo.AddGold(player.playeringameinfo.gold);
+            accountInfo.AddGem(itemInfos.getExp);
+            accountInfo.AddCore(monsterlevel3.rewardAmount);
+        }
+        
+            
+        
     }
 
     private void Update()
